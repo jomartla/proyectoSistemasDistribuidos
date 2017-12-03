5376ee937943ec294dd6515c3bf2c7633686565a
Jujimet <34213556+Jujimet@users.noreply.github.com>
2017-12-03 19:24:07 +0100

    Clase Terminada
    La clase usuario esta terminada con todos sus gets y sets implementados

--- Usuario.java
+++ Usuario.java
@@ -1,4 +1,29 @@
 
 public class Usuario {
+  private String nombreReal;
+	private String nombreUsuario;
+	private String contrasena;
+	private String direccion;
+	
+	
+	public String getNombreReal(){
+		return nombreReal;
+	}
+	
+	public String getNombreUsuario(){
+		return nombreUsuario;
+	}
+	
+	public String getContrasena(){
+		return contrasena;
+	}
+	
+	public String getDireccion(){
+		return direccion;
+	}
+	
+	public void setDireccion(String d){
+		direccion = d;
+	}
 
 }
