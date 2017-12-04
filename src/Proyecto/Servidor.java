2ebc9e3117695bc7143057511969d1d832306985
Jorge <Jorge@LAPTOP-HUU5KUQA.home>
2017-12-04 10:05:02 +0100

    Servidor que lee y guarda fichero.

--- src/Proyecto/Servidor.java
+++ src/Proyecto/Servidor.java
@@ -1,6 +1,80 @@
 package Proyecto;
-import java.net.ServerSocket;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+import Cerrar.Cerrar;
 
 public class Servidor {
-	//ServerSocket ss = new ServerSocket();
+	
+	private static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+	
+	public static void main(String[] args) {
+		
+		ServerSocket servidor=null;
+		
+		leerFichero();
+		try {
+			servidor = new ServerSocket(10000);
+			ExecutorService pool = Executors.newCachedThreadPool();	
+
+			while (true){
+				final Socket cliente = servidor.accept();
+
+				AtenderPeticionServidor atenderCliente = new AtenderPeticionServidor(cliente,usuarios);
+
+				pool.execute(atenderCliente);
+				
+				save();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			Cerrar.cerrar(servidor);
+		}
+	}
+	public static void leerFichero() {
+		
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		
+		try{
+		fis = new FileInputStream("usuarios.dat");
+		ois = new ObjectInputStream(fis);
+		
+		usuarios=(ArrayList<Usuario>) ois.readObject();
+		
+		}catch(FileNotFoundException e){
+			System.out.println("1"+e.getMessage());
+		} catch(IOException e){
+			System.out.println("2"+e.getMessage());
+		}catch(ClassNotFoundException e){
+			System.out.println("3"+e.getMessage());
+		} finally {
+			Cerrar.cerrar(fis);
+			Cerrar.cerrar(ois);
+		}
+	}
+	public static void save() {
+		
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		
+		try{
+		fos=new FileOutputStream("usuarios.dat");
+		oos = new ObjectOutputStream(fos) ;
+		oos.writeObject(usuarios);
+		oos.flush();
+		oos.close();
+		} catch(FileNotFoundException e){
+			System.out.println("1"+e.getMessage());
+		} catch(IOException e){
+			System.out.println("2"+e.getMessage());
+		} finally {
+			Cerrar.cerrar(fos);
+			Cerrar.cerrar(oos);
+		}
+	}
+	
 }
