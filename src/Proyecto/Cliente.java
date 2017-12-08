f337a5a29c7460b672bbc4158bbdedec6578e480
juanjo <juanjo@192.168.1.134>
2017-12-08 13:08:18 +0100

    Cambios en parametros Login, y empezada la imple de AtenderCliente

--- src/Proyecto/Cliente.java
+++ src/Proyecto/Cliente.java
@@ -1,13 +1,20 @@
 package Proyecto;
 
 import java.awt.EventQueue;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.swing.JFrame;
 
+import Cerrar.Cerrar;
+import InterfacesGraficas.Llamar;
 import InterfacesGraficas.Login;
 
 public class Cliente {
@@ -22,11 +29,47 @@ public class Cliente {
 	static String nombreUsuario = null;
 
 	public static void main(String[] args) {
-		Socket servidor;
+		Socket socketServer = null;
+		Socket socketCliente = null;
+		ServerSocket servidorCliente = null;
+		PrintWriter escritura = null;
+		DataInputStream lectura = null;
+		
+		
 		try {
-			servidor = new Socket(ipServidor, puertoServidor);
-			login(servidor);
-			System.out.println("mostrar");
+			socketServer = new Socket(ipServidor, puertoServidor);
+			escritura = new PrintWriter(socketServer.getOutputStream());
+			lectura = new DataInputStream(socketServer.getInputStream());
+			
+			login(escritura, lectura);
+			
+			if(nombreUsuario != null){
+				try {
+					ExecutorService pool = Executors.newCachedThreadPool();	
+					CyclicBarrier barrera;
+					servidorCliente = new ServerSocket(11000);
+					while (true){
+						
+						final Socket cliente = servidorCliente.accept();
+						
+						barrera = new CyclicBarrier(2);
+
+						AtenderPeticionCliente atenderLlamadas = new AtenderPeticionCliente(barrera);
+
+						pool.execute(atenderLlamadas);
+						
+						barrera.await();
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (BrokenBarrierException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}finally{
+					Cerrar.cerrar(servidorCliente);
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -36,12 +79,12 @@ public class Cliente {
 	//El metodo se conecta mediante su usuario y contraseña al servidor
 	//para ello despliega una interfaz grafica, en la cual se nos dara dos opciones, o acceder, lo que equivaldra a logearse,
 	// introduciendo su nombre y contraseña, o registrarse, como nuevo usuario, desplegando para ello una nueva ventana
-	private static void login(Socket s) {
+	private static void login(PrintWriter esc, DataInputStream lec) {
 		CyclicBarrier cb = new CyclicBarrier(2);
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Login frame = new Login(s,nombreUsuario,cb);
+					Login frame = new Login(esc,lec,nombreUsuario);
 					frame.setVisible(true);
 					frame.addWindowListener(new java.awt.event.WindowAdapter() {
 					    @Override
@@ -68,5 +111,19 @@ public class Cliente {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	
+	public static void interfazLlamada(PrintWriter esc, DataInputStream lec){
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Llamar frame = new Llamar(esc,lec);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
